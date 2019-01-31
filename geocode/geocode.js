730f62309cc5646713a82cb906cb2b0e495efd29
Nikola <nksan870@abv.bg>
2019-01-31 23:58:51 +0200

    First commit

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 var geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
-
+  
   request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
--- playground/promise.js
+++ playground/promise.js
@@ -1,12 +1,34 @@
-var somePrpmise = new Promise((resolve, reject) => {
-    setTimeout(()=> {
-        // resolve('Hey it worked!');
-        reject('Unable to fulfill promise');
-    }, 2500);
-});
+// var asyncAdd = (a, b) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => { 
+//             if(typeof a === 'number' && typeof b === 'number'){
+//                 resolve(a + b);
+//             } else {
+//                 reject('Arguments must be numbers.')
+//             }
+//         }, 1500);
+//     });
+// };
 
-somePrpmise.then((message) => {
-    console.log('Success: ', message);
-}, (errorMessage) => {
-    console.log('Fail:', errorMessage);
-});
+// asyncAdd(5, '7').then((res) => {
+//     console.log('Result: ', res);
+//     return asyncAdd(res, 33);
+// }).then((res) => {
+//     console.log('Should be 45', res);
+// }).catch((errorMessage) => {
+//     console.log(errorMessage);
+// });
+
+
+// var somePrpmise = new Promise((resolve, reject) => {
+//     setTimeout(()=> {
+//         // resolve('Hey it worked!');
+//         reject('Unable to fulfill promise');
+//     }, 2500);
+// });
+
+// somePrpmise.then((message) => {
+//     console.log('Success: ', message);
+// }, (errorMessage) => {
+//     console.log('Fail:', errorMessage);
+// });
